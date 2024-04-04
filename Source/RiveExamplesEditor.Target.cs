8af68d127475945e90641c6365936e7d55b0e489
Batname <dadubinin@gmail.com>
2024-04-04 16:12:21 +0200

    5.4 fix

--- Source/RiveExamplesEditor.Target.cs
+++ Source/RiveExamplesEditor.Target.cs
@@ -8,7 +8,7 @@ public class RiveExamplesEditorTarget : TargetRules
 	public RiveExamplesEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "RiveExamples" } );
 	}
